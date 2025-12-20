6be92ae02715e05b6a54c4652a34a9da1431771c
Shabana Qasemi <107318445+Art3mis-s@users.noreply.github.com>
2025-12-20 00:25:49 -0800

    Update RSA.h

--- RSA.h
+++ RSA.h
@@ -11,5 +11,9 @@ unsigned long long gcd(unsigned long long x, unsigned long long y);
 unsigned long long lcm(unsigned long long x, unsigned long long y);
 unsigned long long modInverse(unsigned long long e, unsigned long long lam);
 unsigned long long modExp(unsigned long long base,unsigned long long exp,unsigned long long n);
+unsigned long long bruteFactor(unsigned long long n);
+bool crackRSA(unsigned long long n, unsigned long long e,
+unsigned long long& p, unsigned long long& q,
+unsigned long long& lambda, unsigned long long& d);
 
 #endif // RSA_H_INCLUDED
